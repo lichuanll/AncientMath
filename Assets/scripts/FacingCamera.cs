635532ba04c5e7b6b553212aa5e7d94fb36d736d
121380808 <3175764704@qq.com>
2024-03-19 20:09:29 +0800

    level01

---
+++ Assets/scripts/FacingCamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacingCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Transform[] childs;
+    void Start()
+    {
+        childs = new Transform[transform.childCount];
+        //遍历子物体并存储下来
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            childs[i] = transform.GetChild(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0;i < childs.Length;i++) 
+        {
+            childs[i].rotation = Camera.main.transform.rotation;
+        }
+    }
+}
---
+++ Assets/scripts/PlayerRuninLevel.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRuninLevel : MonoBehaviour
+{
+    public float scaleValue;
+    public float playerMoveSpeed;
+    public Rigidbody2D rigidbody2;
+    public Animator animator;
+
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = Camera.main.transform.position-transform.position;
+
+        scaleValue = this.transform.localScale.x;
+        animator = GetComponent<Animator>();
+        rigidbody2 = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        //Ë®Æ½Öá
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float faceNum = Input.GetAxisRaw("Horizontal");
+        rigidbody2.velocity = new Vector2( horizontal, vertical).normalized*playerMoveSpeed;
+        if (horizontal != 0 || vertical != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (faceNum < 0)
+        {
+            transform.localScale = new Vector3(-scaleValue, transform.localScale.y, transform.localScale.z);
+        }
+        else if (faceNum > 0)
+        {
+            transform.localScale = new Vector3(scaleValue, transform.localScale.y, transform.localScale.z);
+        }
+
+        Camera.main.transform.position = transform.position + offset;
+    }
+}
