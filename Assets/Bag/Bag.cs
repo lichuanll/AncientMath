95e98b334a6457cc8b1377dc077ea91a2b980307
爱因斯川 <13807761+ainschuan@user.noreply.gitee.com>
2024-03-28 20:50:49 +0800

    Add ArtRes And Impelement BagUISystem

--- Assets/Bag/Bag.cs
+++ Assets/Bag/Bag.cs
@@ -13,22 +13,31 @@ public class Bag : MonoBehaviour
     public Sprite Plane;
     public int RowCount;
     public int ColCount;
+    private int MaxCount=30;
     public void AddItem(Item item)
     {
-        Items.Add(item);
+        if (Items.Count < MaxCount)
+        {
+            Items.Add(item);
+        }
     }
 
     public void ShowBag()
     {
         gameObject.SetActive(true);
         GameObject ItemUI;
+       
         for (int i = 0; i < Items.Count; i++)
         {
             ItemUI = this.transform.GetChild(i).gameObject;
             ItemUI.SetActive(true);
             Image ico = ItemUI.GetComponent<Image>();
             ico.sprite = Items[i].Ico;
-            ItemUI.GetComponent<ItemButton>().Intro = Items[i].Intro;
+            ItemButton itemButton = ItemUI.GetComponent<ItemButton>();
+            itemButton.Intro = Items[i].Intro;
+            itemButton.Name = Items[i].Name;
+            itemButton.Image = Items[i].Image;
         }
+       // print(Items.Count);
     }
 }
